836fb2a9ca80377ea0fc6b7f46bdca7ad4251dda
Eudy Arias <>
2022-09-11 14:10:05 -0400

    Para este ejercicio tendréis que crear una función que reciba un precio y devuelva el precio con el IVA incluido.

--- Curso de Java Básico/EjerciciosTema1/src/Main.java
+++ Curso de Java Básico/EjerciciosTema1/src/Main.java
@@ -1,8 +1,17 @@
 import Services.FirstClass;
 
+import java.util.function.Function;
+
 public class Main {
     public static void main(String[] args) {
 
         FirstClass.tiposDatos();
+        var montoConIva = PrecioConIva(190000);
+        System.out.println( java.text.MessageFormat.format("Monto con IVA incluido {0}" ,montoConIva));
+
+    }
+    //Para este ejercicio tendréis que crear una función que reciba un precio y devuelva el precio con el IVA incluido.
+    public static double PrecioConIva(double precio){
+        return (precio*0.18)+precio;
     }
 }
